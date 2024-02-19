0a1cae31d5a3c8c7af49fe6302c93db0a7233944
shankarchawla1776 <shankarchawla9101112@gmail.com>
2024-02-18 20:54:01 -0700

    added layer control

---
+++ map_gen/main.py
@@ -0,0 +1,104 @@
+import folium
+from folium import plugins
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from folium.plugins import FastMarkerCluster, MarkerCluster
+import io 
+import boto3 
+import os                                                                                                                                                                                                          
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+import json
+
+
+
+load_dotenv(Path(".env"))
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+bucket_name = 'dermod'
+s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+us_center = [37.0902, -95.7129]
+m = folium.Map(location=us_center, zoom_start=4, zoom_control = False, scrollWheelZoom=True)
+
+
+
+callback_0 = """\
+function (row) { 
+    var icon, marker;
+    icon = L.AwesomeMarkers.icon({
+        icon: "map-marker", markerColor: "red"});
+    marker = L.marker(new L.LatLng(row[0], row[1]));
+    marker.setIcon(icon);
+    marker.bindPopup("Individual Wind Turbine")
+    return marker;
+};
+"""
+callback_1 = """
+function (row) {
+    var icon, marker;
+    icon = L.AwesomeMarkers.icon({
+        icon: "map-marker", markerColor: "blue"});
+    marker = L.marker(new L.LatLng(row[0], row[1]));
+    marker.setIcon(icon);
+    marker.bindPopup("Utility-Scale Photovoltaic Power Station ")
+    return marker;
+};
+"""
+callback_2 = """
+function (row) {
+    var icon, marker;
+    icon = L.AwesomeMarkers.icon({
+        icon: "sun", markerColor: "green"});
+    marker = L.marker(new L.LatLng(row[0], row[1]));
+    marker.setIcon(icon);
+    marker.bindPopup("Distributed Solar Unit")
+    return marker;
+};
+"""
+resp_wind = s3_client.get_object(Bucket=bucket_name, Key='wind_energy_2.csv')
+
+wind_data = resp_wind['Body'].read().decode('utf-8')
+wind_df = pd.read_csv(io.StringIO(wind_data))
+CAISO_wind = wind_df[wind_df["t_state"] == "CA"]
+xlong_values = CAISO_wind["xlong"].tolist()
+ylat_values = CAISO_wind["ylat"].tolist()
+locs_wind = CAISO_wind.apply(lambda row: (row["ylat"], row["xlong"]), axis=1).tolist()
+
+
+resp_util_sol = s3_client.get_object(Bucket=bucket_name, Key='utility_solar (1).csv')
+util_sol_data = resp_util_sol['Body'].read().decode('utf-8')
+util_sol_df = pd.read_csv(io.StringIO(util_sol_data), on_bad_lines='skip', delimiter='\t')
+CAISO_sol = util_sol_df[util_sol_df["state"] == "CA"]
+locs_sol = CAISO_sol.apply(lambda row: (row["latitude"], row["longitude"]), axis=1).tolist()
+
+
+
+resp_distr_sol = s3_client.get_object(Bucket=bucket_name, Key='CASIO_coords.csv')
+distr_sol_data = resp_distr_sol['Body'].read().decode('utf-8')
+CAISO_coords_sol = pd.read_csv(io.StringIO(distr_sol_data), on_bad_lines='skip')
+lats_distr = CAISO_coords_sol["latitude"]
+longs_distr = CAISO_coords_sol["longitude"]
+locs_distr_sol = CAISO_coords_sol.apply(lambda row: (row["latitude"], row["longitude"]), axis=1).tolist()
+
+max_cluster_distance = 5
+
+
+
+marker_cluster_sol = FastMarkerCluster(data=locs_sol, callback=callback_1).add_to(m)
+marker_cluster_wind = FastMarkerCluster(data=locs_wind, callback=callback_0).add_to(m)
+marker_cluster_distr_sol = FastMarkerCluster(data=locs_distr_sol, callback=callback_2, options={'distance': max_cluster_distance}).add_to(m)
+
+
+
+folium.LayerControl().add_to(m)
+m.add_child(marker_cluster_distr_sol)
+m.add_child(marker_cluster_wind)
+m.add_child(marker_cluster_sol)
+m.save("map_CAISO.html")
+
+CAISO_sol = util_sol_df[util_sol_df["state"] == "CA"]
+
+ylat = util_sol_df["latitude"].tolist()
---
+++ map_gen/testmap.py
@@ -0,0 +1,67 @@
+import folium
+from folium import plugins
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from folium.plugins import FastMarkerCluster, MarkerCluster
+import io 
+import boto3 
+import os                                                                                                                                                                                                          
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+import json
+
+load_dotenv(Path(".env"))
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+bucket_name = 'dermod'
+
+s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+us_center = [37.0902, -95.7129]
+m = folium.Map(location=us_center, zoom_start=4, zoom_control=False, scrollWheelZoom=True)
+
+wind_group = folium.FeatureGroup(name='Wind Energy')
+solar_group = folium.FeatureGroup(name='Utility-Scale Solar')
+distr_solar_group = folium.FeatureGroup(name='Distributed Solar')
+
+def create_marker(row, icon_color, popup_text):
+    icon = folium.Icon(color=icon_color)
+    marker = folium.Marker(location=(row["latitude"], row["longitude"]), icon=icon, popup=popup_text)
+    return marker
+
+resp_wind = s3_client.get_object(Bucket=bucket_name, Key='wind_energy_2.csv')
+wind_data = resp_wind['Body'].read().decode('utf-8')
+wind_df = pd.read_csv(io.StringIO(wind_data))
+CAISO_wind = wind_df[wind_df["t_state"] == "CA"]
+for _, row in CAISO_wind.iterrows():
+    wind_group.add_child(create_marker(row, 'red', 'Individual Wind Turbine'))
+
+
+resp_util_sol = s3_client.get_object(Bucket=bucket_name, Key='utility_solar (1).csv')
+util_sol_data = resp_util_sol['Body'].read().decode('utf-8')
+util_sol_df = pd.read_csv(io.StringIO(util_sol_data), on_bad_lines='skip', delimiter='\t')
+CAISO_sol = util_sol_df[util_sol_df["state"] == "CA"]
+for _, row in CAISO_sol.iterrows():
+    solar_group.add_child(create_marker(row, 'blue', 'Utility-Scale Photovoltaic Power Station'))
+
+
+resp_distr_sol = s3_client.get_object(Bucket=bucket_name, Key='CASIO_coords.csv')
+distr_sol_data = resp_distr_sol['Body'].read().decode('utf-8')
+CAISO_coords_sol = pd.read_csv(io.StringIO(distr_sol_data), on_bad_lines='skip')
+max_cluster_distance = 5
+for _, row in CAISO_coords_sol.iterrows():
+    distr_solar_group.add_child(create_marker(row, 'green', 'Distributed Solar Unit'))
+
+# Adding feature groups to the map
+m.add_child(wind_group)
+m.add_child(solar_group)
+m.add_child(distr_solar_group)
+
+# Adding layer control
+folium.LayerControl().add_to(m)
+
+# Save the map
+m.save("map_CAISO.html")
