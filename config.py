e6de47f68ccefa508cebc51121ef1fbe8da5d2d9
shankarchawla1776 <shankarchawla9101112@gmail.com>
2024-02-20 22:44:12 -0700

    postgres config

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 from configparser import ConfigParser 
 
-# def config(filename="database.ini")
+def config(filename="db.ini", section="postgresql"): 
+    parser = ConfigParser()
+    parser.read(filename)
+    der_sdb = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for i in params:
+            der_sdb[i[0]] = i[1]
+    else: 
+        raise Exception(f'section not found in db file.'.format(section, filename))
+    return der_sdb
---
+++ connect.py
@@ -0,0 +1,23 @@
+import psycopg2
+from config import config
+
+def connection(): 
+    connect = None 
+    try:
+        params = config()
+        connect = psycopg2.connect(**params)
+        cursor = connect.cursor()
+        cursor.execute('SELECT version()')
+        version = cursor.fetchone()
+        print(version)
+        cursor.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally: 
+        if connect is not None: 
+            connect.close()
+            print('db connection closed')
+
+if __name__ == "__main__": 
+    connection()
+
