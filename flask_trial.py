d2f6558e6e2b4059827a3648c165565e53833202
shankarchawla1776 <shankarchawla9101112@gmail.com>
2024-02-21 20:06:34 -0700

    minor changes

--- flask_trial.py
+++ flask_trial.py
@@ -14,7 +14,7 @@ def fetch_data(page=1, per_page=100):
         offset = (page - 1) * per_page
         cursor.execute('SELECT xlong, ylat FROM "USWTDB" LIMIT %s OFFSET %s', (per_page, offset))
         rows = cursor.fetchall()
-        data = [[row[0], row[1]] for row in rows]  # Convert rows to list of lists
+        data = [[row[0], row[1]] for row in rows]
         return data
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
