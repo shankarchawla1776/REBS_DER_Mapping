72e6c54925c007ff137c01d5760dde0d79cf89ba
shankarchawla1776 <shankarchawla9101112@gmail.com>
2024-03-01 21:58:38 -0700

    update

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ import pandas as pd
 from holoviews.operation.datashader import datashade
 import matplotlib.pyplot as plt
 import numpy as np 
-from holoviews.element.tiles import EsriImagery
+from holoviews.element.tiles import EsriImagery, OSM, CartoLight
 from dotenv import load_dotenv
 import boto3
 from pathlib import Path
 import io 
-# from map_gen.main import CAISO_coords_sol
 
+ds_count = 10 
+checkboxes = []
 load_dotenv(Path(".env"))
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
@@ -30,39 +31,50 @@ distr_sol_data = resp_distr_sol['Body'].read().decode('utf-8')
 CAISO_coords_sol = pd.read_csv(io.StringIO(distr_sol_data), on_bad_lines='skip')
 
 hv.extension('bokeh', logo=False)
+
+def update_opacity(active, alpha):
+    if active:
+        return alpha
+    else:
+        return 0
+
+alpha_values = [0.8] * ds_count
+
+def update_plot(*checkbox_values):
+    alpha = [update_opacity(active, alpha) for active, alpha in zip(checkbox_values, alpha_values)]
+    decimated_combined_points.opts(alpha=alpha)
+
+d_names = ['Wind', 'Solar', 'Hydro', 'Geothermal', 'Nuclear', 'Biomass', 'Coal', 'Oil', 'Gas', 'Other']
+
+
+for i in range(ds_count): 
+    d_types = pn.widgets.Checkbox(name=d_names[i], value=True, sizing_mode='fixed', layout='column', width=400, height=20)
+    checkboxes.append(d_types)
+
 data = fetch_data(wind=True)
 data2 = CAISO_coords_sol
-df = pd.DataFrame(data, columns=['latitude', 'longitude'])
+dfs = [pd.DataFrame(data, columns=['latitude', 'longitude']), 
+              pd.DataFrame(data2, columns=['latitude', 'longitude'])]
+              
+df1 = pd.DataFrame(data, columns=['latitude', 'longitude'])
 df2 = pd.DataFrame(data2, columns=['latitude', 'longitude'])
-df['x'], df['y'] = lnglat_to_meters(df['longitude'], df['latitude'])
+
+df1['x'], df1['y'] = lnglat_to_meters(df1['longitude'], df1['latitude'])
+
 df2['x'], df2['y'] = lnglat_to_meters(df2['longitude'], df2['latitude'])
 
-points = hv.Points(df, ['x', 'y'])
-points2 = hv.Points(df2, ['x', 'y'])
 
-decimated_points = decimate(points)
-# decimated_points2 = decimate(points2)
-map = EsriImagery()
-
-# Create a shaded plot
-# shaded_plot = shade(decimated_points, cmap=colorcet.fire)
+points_combined = pd.concat([df1, df2])
+combined_points = hv.Points(points_combined, ['x', 'y'])
 
 
-stream2_visibility = pn.widgets.Checkbox(name='Show Solar', value=True)
+decimated_combined_points = decimate(combined_points)
+decimated_combined_points.opts(alpha=0.8)
+map = OSM()
 
-# def toggle_stream2_visibility(active):
-#     if active:
-#         shaded_plot = map * datashade(points, cmap=colorcet.fire) * datashade(points2, cmap=colorcet.fire)
-#     else:
-#         shaded_plot = map * datashade(points, cmap=colorcet.fire)
-#     pn.pane.HoloViews(shaded_plot).servable()
-
-shaded_plot = map * datashade(points, cmap=colorcet.fire)
+shaded_plot = map * datashade(decimated_combined_points, cmap=colorcet.kb) # => ,cmap=colorcet.fire
 
 shaded_plot.opts(width=1250, height=800)
-
-# # Add the callback function to the widget
-# stream2_visibility.param.watch(toggle_stream2_visibility, 'value')
 
 
 dashboard = pn.Column(
@@ -81,17 +93,17 @@ pricing = pn.widgets.FloatSlider(
     name='Price', start=0, end=1, value=0.5
 ).servable(target='sidebar')
 
-d_names = ['Wind', 'Solar', 'Hydro', 'Geothermal', 'Nuclear', 'Biomass', 'Coal', 'Oil', 'Gas', 'Other']
-checkboxes = []
 
-for i in range(10): 
-    d_types = pn.widgets.Checkbox(name=d_names[i], value=True, sizing_mode='fixed', layout='column', width=400, height=20)
-    checkboxes.append(d_types)
 
 for i in checkboxes: 
     i.servable(target='sidebar')
-
-# checkbox_column = pn.Column(*checkboxes, align="center", sizing_mode='stretch_both')
-# checkbox_column.servable(target='sidebar')
-                         
+          
 dashboard.servable(title="Distributed Energy Resources Mapping")
+
+
+# def toggle_stream2_visibility(active):
+#     if active:
+#         shaded_plot = map * datashade(points, cmap=colorcet.fire) * datashade(points2, cmap=colorcet.fire)
+#     else:
+#         shaded_plot = map * datashade(points, cmap=colorcet.fire)
+#     pn.pane.HoloViews(shaded_plot).servable()
